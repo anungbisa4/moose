6c9eb3af1f900120430c1c69e390a355edb9bbff
anunganinditha <anung.bisa4@gmail.com>
2023-03-02 16:25:40 +0700

    first init

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,60 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import * as React from "react";
+import PropTypes from "prop-types";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import createEmotionServer from "@emotion/server/create-instance";
+import theme, { roboto } from "@/helpers/styles/theme";
+import createEmotionCache from "@/helpers/styles/createEmotionCache";
 
-export default function Document() {
+export default function MyDocument(props) {
+  const { emotionStyleTags } = props;
+
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="en" className={roboto.className}>
+      <Head>
+        {/* PWA primary color */}
+        <meta name="theme-color" content={theme.palette.primary.main} />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="emotion-insertion-point" content="" />
+        {emotionStyleTags}
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+  const originalRenderPage = ctx.renderPage;
+
+  const cache = createEmotionCache();
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) =>
+        function EnhanceApp(props) {
+          return <App emotionCache={cache} {...props} />;
+        },
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(" ")}`}
+      key={style.key}
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+
+  return {
+    ...initialProps,
+    emotionStyleTags,
+  };
+};
+
+MyDocument.propTypes = {
+  emotionStyleTags: PropTypes.array.isRequired,
+};
